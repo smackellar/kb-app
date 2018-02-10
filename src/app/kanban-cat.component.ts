b99c50b4d13c31f956ab95243b15f538993e8a48
Stuart MacKellar <stu_mackellar@hotmail.com>
2018-02-10 19:53:35 +0000

    Tidy up cat structure

--- src/app/kanban-cat.component.ts
+++ src/app/kanban-cat.component.ts
@@ -4,10 +4,12 @@ import { Item } from './item';
 import { CatType } from './cat-type';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Rx';
+import { ItemService } from './item.service';
 
 @Component({
   selector: 'cat',
-  templateUrl: './kanban-cat.component.html'
+  templateUrl: './kanban-cat.component.html',
+  styleUrls: [ './kanban.component.css' ]
 })
 
 export class KanbanCat implements OnInit {
@@ -20,7 +22,7 @@ export class KanbanCat implements OnInit {
     this.origCat = this.cat;
   }
 
-  constructor(private elementRef: ElementRef) {
+  constructor(private itemService: ItemService, private elementRef: ElementRef) {
     const eventStream = Observable.fromEvent(elementRef.nativeElement, 'keyup')
         .map(() => this.cat)
         .debounceTime(500)
@@ -41,6 +43,43 @@ export class KanbanCat implements OnInit {
       }
     }
 
+  }
+
+  removeCat(catToRemove: string): void {
+    console.log("Remove cat: " + catToRemove);
+    // check at least one cat
+    if (this.catTypeSelect.cats.length == 1){
+      console.log("Last remaining cat - do not remove");
+      return;
+    }
+
+    // update items to first cat
+    for (let item of this.items){
+      if (item[this.catTypeSelect.name] == catToRemove){
+        item[this.catTypeSelect.name] = (this.catTypeSelect.cats[0] == item[this.catTypeSelect.name] ? this.catTypeSelect.cats[1] : this.catTypeSelect.cats[0]);
+        this.updateItem(item);
+      }
+    }
+
+    // remove the cat
+    this.catTypeSelect.cats.splice(this.catTypeSelect.cats.indexOf(catToRemove),1);
+  }
+
+  onDrop(event : any, cat : string): void {
+    // loop through items to find the one that needs to be updated
+    for (let item of this.items){
+      if (item.id == parseInt(event)){
+        item[this.catTypeSelect.name] = cat;
+        console.log(item[this.catTypeSelect.name]);
+        this.updateItem(item);
+      }
+    }
+  }
+
+
+  updateItem(item: Item){
+    this.itemService.update(item).then(() => this.itemService.getItems()
+      .then(items => this.items = items));
   }
 
 }
--- src/app/kanban.component.ts
+++ src/app/kanban.component.ts
@@ -42,57 +42,17 @@ export class KanbanComponent implements OnInit {
     }
   }
 
-  private findUniqueCatName(name: string): string {
-    var suffixCount = 0;
-    while(this.catTypeSelect.cats.indexOf(suffixCount == 0 ? name : name + " (" + suffixCount + ")") > 0){
-      suffixCount++;
-      console.log(name + " (" + suffixCount + ")");
-    }
-    return suffixCount == 0 ? name : name + " (" + suffixCount + ")";
-  }
-
   addCat(): void {
-    console.log("New cat: " + this.newCat);
-    if (this.newCat.length == 0) {
-      this.newCat = "New " + this.catTypeSelect.name;
+    if (this.newCat.length == 0){
+      console.log("Category is empty" + this.newCat);
+      return;
     }
-    this.catTypeSelect.cats.push(this.findUniqueCatName(this.newCat));
+    if (this.catTypeSelect.cats.includes(this.newCat)) {
+      console.log("Category exists: " + this.newCat);
+      return;
+    }
+    this.catTypeSelect.cats.push(this.newCat);
     this.newCat = "";
   }
 
-  removeCat(catToRemove: string): void {
-    console.log("Remove cat: " + catToRemove);
-    // check at least one cat
-    if (this.catTypeSelect.cats.length == 1){
-      console.log("Last remaining cat - do not remove");
-      return;
-    }
-
-    // update items to first cat
-    for (let item of this.items){
-      if (item[this.catTypeSelect.name] == catToRemove){
-        item[this.catTypeSelect.name] = (this.catTypeSelect.cats[0] == item[this.catTypeSelect.name] ? this.catTypeSelect.cats[1] : this.catTypeSelect.cats[0]);
-        this.updateItem(item);
-      }
-    }
-
-    // remove the cat
-    this.catTypeSelect.cats.splice(this.catTypeSelect.cats.indexOf(catToRemove),1);
-  }
-
-  onDrop(event : any, cat : string): void {
-    // loop through items to find the one that needs to be updated
-    for (let item of this.items){
-      if (item.id == parseInt(event)){
-        item[this.catTypeSelect.name] = cat;
-        console.log(item[this.catTypeSelect.name]);
-        this.updateItem(item);
-      }
-    }
-  }
-
-  updateItem(item: Item){
-    this.itemService.update(item).then(() => this.itemService.getItems()
-      .then(items => this.items = items));
-  }
 }
