c4494642afa6958dadf448f6a291b9e77251311b
Stuart MacKellar <stu_mackellar@hotmail.com>
2018-02-18 12:24:19 +0000

    Make cards conditionally editable

--- src/app/item-card.component.ts
+++ src/app/item-card.component.ts
@@ -1,6 +1,7 @@
 import 'rxjs/add/operator/switchMap';
 import { Component, OnInit, Input }      from '@angular/core';
 import { Location }               from '@angular/common';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { Item }         from './item';
 import { ItemService }  from './item.service';
@@ -12,13 +13,18 @@ import { ItemService }  from './item.service';
 export class ItemCardComponent implements OnInit {
   @Input() item: Item;
   @Input() expand: boolean;
+  editable: boolean = false;
 
   constructor(
     private itemService: ItemService,
+    private route: ActivatedRoute,
     private location: Location
-  ) {}
+
+  ) {
+  }
 
   ngOnInit(): void {
+    this.editable = this.route.snapshot.url[0].path!="detail";
   }
 
 }
