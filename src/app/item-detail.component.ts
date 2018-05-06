7be5cc3ba59ae4ff0c1c06f0f4dbe7b9460d6646
Stuart MacKellar <stu_mackellar@hotmail.com>
2018-05-06 22:39:46 +0100

    Allow editing of item values

--- src/app/item-detail.component.ts
+++ src/app/item-detail.component.ts
@@ -4,21 +4,27 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Location }               from '@angular/common';
 
 import { Item }         from './item';
+import { TypeDef }  from './type-def';
 import { ItemService }  from './item.service';
+import { DefCurrentService }  from './def-current.service';
+
 @Component({
   selector: 'item-detail',
   templateUrl: './item-detail.component.html'
 })
 export class ItemDetailComponent implements OnInit {
   item: Item;
+  defSelect: TypeDef;
 
   constructor(
     private itemService: ItemService,
     private route: ActivatedRoute,
+    private defCurrentService: DefCurrentService,
     private location: Location
   ) {}
 
   ngOnInit(): void {
+    this.defSelect = this.defCurrentService.typeDef;
     this.route.params
       .switchMap((params: Params) => this.itemService.getItem(+params['id']))
       .subscribe(item => this.item = item);
@@ -31,6 +37,10 @@ export class ItemDetailComponent implements OnInit {
     this.itemService.delete(this.item)
     .then(() => this.location.back());
 
+  }
+  updateField(value, fieldId): void {
+    console.log("===" + value);
+    this.item.values[fieldId] = value;
   }
   goBack(): void {
     this.location.back();
